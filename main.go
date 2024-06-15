37a079ec57480932f8eb0a84ed118f7ae4d56025
Dom <112484879+BonelessWater@users.noreply.github.com>
2024-06-15 19:15:04 -0400

    Began Connection protocol

--- main.go
+++ main.go
@@ -3,18 +3,29 @@ package main
 import (
 	//"crypto/sha256"
 
+	"bufio"
 	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strconv"
+	"sync"
 	"time"
+
+	"github.com/davecgh/go-spew/spew"
 )
 
 type Block struct {
 	Index int
-	Timestamp int64
+	Timestamp string
+	Data int
 	//NewTransactions []Transaction
-	Merkleroot [32]byte
-	PrevHash [32]byte
-	Hash [32]byte
+	//Merkleroot [32]byte
+	PrevHash string
+	Hash string
 }
 type Transaction struct { 
 	sender [4]byte
@@ -23,37 +34,44 @@ type Transaction struct {
 }
 var Blockchain []Block // later this will be initiliazed by json package
 
+var bcServer chan []Block // bcServer handles incoming concurrent Blocks
+var mutex = &sync.Mutex{}
+
+
 func main() {
 
 	if len(Blockchain) == 0 { // Create genesis block if Blockchain is empty
 		b := Block {
 			Index: 0,
-			Timestamp: time.Now().Unix(),
+			Timestamp: time.Now().Format(time.RFC3339),
 		}
-		 // Convert struct to byte slice (JSON representation)
-		jsonData, err := json.Marshal(b)
-		if err != nil {
-			panic(err)
-		}
-		b.Hash = sha256.Sum256(jsonData)
+		b.Hash = calculateHash(b)
+		spew.Dump(b)
+		Blockchain = append(Blockchain, b)
 	}
 
+	tcpPort := os.Getenv("PORT") // gets environment variable
+
+	server, err := net.Listen("tcp", ":"+tcpPort) // start TCP and serve TCP server
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("TCP  Server Listening on port :", tcpPort)
+	defer server.Close()
+
+	for {
+		conn, err := server.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleConn(conn)
+	}
 	// Fetch json
 	
 	
 
 
 	// create new block
-
-
-	//var m_value []int 
-	//var rsa_bl []int
-	//pointer := 0
-	//for i := 0; i < len(asciiValues); i++{ // makes sure the values of m do not exceed n
-	//	if m_value[pointer]
-	//		m_value[pointer] = asciiValues[i]
-	//}
-
 
 	//hash := sha256.New() // Get the hash of the message
 	//hash.Write([]byte(msg))
@@ -63,3 +81,105 @@ func main() {
 }
 
 // make method for verifying if a foreign node added the correct amount of coins to their account
+
+func handleConn(conn net.Conn) {
+
+	defer conn.Close()
+
+	io.WriteString(conn, "Who do you want to send money to: ")
+
+	scanner := bufio.NewScanner(conn)
+
+	// take in data from stdin and add it to blockchain after conducting necessary validation
+	go func() {
+		for scanner.Scan() {
+			data, err := strconv.Atoi(scanner.Text())
+			if err != nil {
+				log.Printf("%v not a number: %v", scanner.Text(), err)
+				continue
+			}
+			newBlock, err := generateBlock(Blockchain[len(Blockchain)-1], data)
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			if isBlockValid(newBlock, Blockchain[len(Blockchain)-1]) {
+				newBlockchain := append(Blockchain, newBlock)
+				replaceChain(newBlockchain)
+			}
+
+			bcServer <- Blockchain
+			io.WriteString(conn, "\nEnter a new number:")
+		}
+	}()
+
+	// simulate receiving broadcast
+	go func() {
+		for {
+			time.Sleep(30 * time.Second)
+			mutex.Lock()
+			output, err := json.Marshal(Blockchain)
+			if err != nil {
+				log.Fatal(err)
+			}
+			mutex.Unlock()
+			io.WriteString(conn, string(output))
+		}
+	}()
+
+	for _ = range bcServer {
+		spew.Dump(Blockchain)
+	}
+
+}
+
+// make sure block is valid by checking index, and comparing the hash of the previous block
+func isBlockValid(newBlock, oldBlock Block) bool {
+	if oldBlock.Index+1 != newBlock.Index {
+		return false
+	}
+
+	if oldBlock.Hash != newBlock.PrevHash {
+		return false
+	}
+
+	if calculateHash(newBlock) != newBlock.Hash {
+		return false
+	}
+
+	return true
+}
+
+// make sure the chain we're checking is longer than the current blockchain
+func replaceChain(newBlocks []Block) {
+	mutex.Lock()
+	if len(newBlocks) > len(Blockchain) {
+		Blockchain = newBlocks
+	}
+	mutex.Unlock()
+}
+
+// SHA256 hasing
+func calculateHash(block Block) string {
+	record := string(block.Index) + block.Timestamp + string(block.Data) + block.PrevHash
+	h := sha256.New()
+	h.Write([]byte(record))
+	hashed := h.Sum(nil)
+	return hex.EncodeToString(hashed)
+}
+
+// create a new block using previous block's hash
+func generateBlock(oldBlock Block, Data int) (Block, error) {
+
+	var newBlock Block
+
+	t := time.Now()
+
+	newBlock.Index = oldBlock.Index + 1
+	newBlock.Timestamp = t.String()
+	newBlock.Data = Data
+	newBlock.PrevHash = oldBlock.Hash
+	newBlock.Hash = calculateHash(newBlock)
+
+	return newBlock, nil
+}
